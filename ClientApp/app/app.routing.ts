9fdc7209a93be578ae06c4fbd5d2dbc59f17a75d
Quinntyne Brown <quinntynebrown@gmail.com>
2017-10-21 13:33:16 -0400

    initial commit

---
+++ ClientApp/app/app.routing.ts
@@ -0,0 +1,19 @@
+﻿import { Routes, RouterModule } from '@angular/router';
+import { DonationPageComponent } from "./pages/donation-page.component";
+import { LandingPageComponent } from "./pages/landing-page.component";
+
+export const ROUTES = [
+    {
+        path: '',
+        pathMatch: 'full',
+        component: LandingPageComponent
+    },
+    {
+        path: 'donation',
+        component: DonationPageComponent
+    }
+]
+
+export const RoutingModule = RouterModule.forRoot([
+    ...ROUTES
+]);
---
+++ ClientApp/app/pages/donation-page.component.ts
@@ -0,0 +1,8 @@
+import {Component, ViewEncapsulation} from "@angular/core";
+
+@Component({
+    templateUrl: "./donation-page.component.html",
+    styleUrls: ["./donation-page.component.css"],
+    selector: "ce-donation-page",
+})
+export class DonationPageComponent { }
